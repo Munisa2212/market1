43e563689045b5076859705f6afcc1ac0b1659dc
faridas <Kiyomiddinovas@gmail.com>
2025-02-03 21:54:55 +0500

    fixed bugs

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-let box = document.querySelector(".root")
+let box = document.querySelector(".root");
+
+let t = localStorage.getItem("array");
+if (!t) {
+  localStorage.setItem("array", JSON.stringify([]));
+}
 
 const instance = axios.create({
-    baseURL: "https://679a1481747b09cdcccd9973.mockapi.io",
+  baseURL: "https://679a1481747b09cdcccd9973.mockapi.io",
 });
 
 function show(arr) {
-    box.innerHTML = ""
-    arr.forEach((element) => {
-        box.insertAdjacentHTML("beforeend", `
+  box.innerHTML = "";
+  arr.forEach((element) => {
+    box.insertAdjacentHTML(
+      "beforeend",
+      `
             <div class="card">
                 <img src="${element.photo}}">
                 <h1>${element.name}</h1>
@@ -15,19 +22,20 @@ function show(arr) {
                 <button onclick="deletePro('${element.id}')">Delete</button>
                 <button onclick="buyItem('${element.id}')">Buy</button>
             </div>
-        `);
-    });
+        `
+    );
+  });
 }
 
-instance.get("/two").then((res)=>show(res.data))
+instance.get("/two").then((res) => show(res.data));
 
-function deletePro(son){
-    instance.delete(`/two/${son}`).then((res)=>location.reload())
+function deletePro(son) {
+  instance.delete(`/two/${son}`).then((res) => location.reload());
 }
 
-function buyItem(id){
-    let t = localStorage.getItem("array")
-    let arr = JSON.parse(t)
-    arr.push(id)
-    localStorage.setItem("array", JSON.stringify(arr))
+function buyItem(id) {
+  let t = localStorage.getItem("array");
+  let arr = JSON.parse(t);
+  arr.push(id);
+  localStorage.setItem("array", JSON.stringify(arr));
 }
